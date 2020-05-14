2b76c0809404b860cecf3c95633a24845fc300c8
tahash <tahash.miranda@quantiphi.com>
2020-05-14 22:40:21 +0530

    Bug fixes

--- handler.go
+++ handler.go
@@ -18,13 +18,11 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 		url := r.URL.Path
 		fmt.Println("%T %V", url, url)
 		if val, ok := pathsToUrls[url]; ok {
-			http.Redirect(w, r, url, 301)
+			http.Redirect(w, r, val, http.StatusFound)
 			return
 		}
 		fallback.ServeHTTP(w, r)
-
-
-	})
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
